b4a611c2adca397bc70e2d2afa6efd853d4a6ebc
Dreroyce02 <dreroyce02@gmail.com>
2020-05-20 09:38:00 -0400

    added test function to physics.py

--- physics.py
+++ physics.py
@@ -34,3 +34,14 @@ def gForce(b1, b2, dt):
     b1.mom[0] = F[0]*dt+b1.mom[0]
     b1.mom[1] = F[1]*dt+b1.mom[1]
         
+def test():
+    b1 = Body(20, [10, 20], [0, 0])
+    b2 = Body(100, [70, -10], [0, 0])
+    gForce = (b1, b2, .001)
+    gForce = (b2, b1, .001)
+    b1.vmove(.001)
+    b2.vmove(.001)
+    print(b1.p, b2.p)
+
+if __name__=='__main__':
+    test()
