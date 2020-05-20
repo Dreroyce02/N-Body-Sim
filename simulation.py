21baaab6f4e9772fe5939a5da1694ba82515cd8d
Dreroyce02 <dreroyce02@gmail.com>
2020-05-20 10:10:37 -0400

    values for the bodies are determined at random

--- simulation.py
+++ simulation.py
@@ -1,1 +1,12 @@
 from physics import Body, gForce
+import random
+
+n_of_bodies = 10
+bodies = []
+
+for thing in range(n_of_bodies):
+    b = Body(random.randint(1, 100),
+             [random.randint(-100,100), random.randint(-100,100)],
+             [random.randint(-100,100), random.randint(-100,100)]
+             )
+    bodies.append(b)
