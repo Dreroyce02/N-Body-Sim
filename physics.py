c30ce50b92ab595d222f0f15bd933c29c4b78c1a
Dreroyce02 <dreroyce02@gmail.com>
2020-05-20 09:18:54 -0400

    Added a force function

--- physics.py
+++ physics.py
@@ -1,23 +1,36 @@
 import math
 
+G = 1
+
 class Body:
 
-    def __init__(self, mass, position, velocity):
+    def __init__(self, mass, position, momentum):
         self.m = mass
         self.p = position
-        self.v = velocity
+        self.mom = momentum
 
     def pmove(self, dx, dy):
         self.p[0] = self.p[0]+dx
         self.p[1] = self.p[1]+dy
 
     def vmove(self, dt):
-        self.p[0] = self.v[0]*dt+self.p[0]
-        self.p[1] = self.v[1]*dt+self.p[1]
+        self.p[0] = (self.mom[0]/self.m)*dt+self.p[0]
+        self.p[1] = (self.mom[1]/self.m)*dt+self.p[1]
 
 
-obj_1 = Body(20,[5,6],[2,5])
-obj_1.pmove(2,2)
-obj_1.vmove(2)
+def mag(p1, p2):
+    a, b = p1[0], p1[1]
+    c, d = p2[0], p2[1]
+    return math.pow(math.pow(a-c, 2)+math.pow(b-d, 2), .5)
 
-print(obj_1.m,obj_1.p,obj_1.v)
+def gForce(b1, b2, dt):
+    p1, m1 = b1.p, b1.m
+    p2, m2 = b2.p, b1.m2
+    r = [p2[0]-p1[0], p2[1]-p1[1]]
+    r_mag = mag(p1, p2)
+    F = [((G*m1*m2)/(math.pow(r_mag, 3)))*r[0],
+         ((G*m1*m2)/(math.pow(r_mag, 3)))*r[1]
+        ]
+    b1.mom[0] = F[0]*dt+b1.mom[0]
+    b1.mom[1] = F[1]*dt+b1.mom[1]
+        
